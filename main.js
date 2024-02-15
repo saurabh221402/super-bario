26c362111638aebe3e65eb9ed6e1cafd2af9c8cc
saurabh gupta <sg072106@gmail.com>
2024-02-15 19:56:20 +0530

    player creation done

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-alert("ds");
+ 
 const canva=document.querySelector('canvas');
 
 const c=canva.getContext('2d');
 
-console.log(c);
+//defining the dimension of canvas
+canva.width = innerWidth;
+canva.height = innerHeight;
+//creating player class
+class Player {
+    constructor(){
+        this.position ={
+            x:100,
+            y:100
+        }
+        this.height =50
+        this.width =50
+    }
+    create(){
+        c.fillStyle='red';
+        c.fillRect(this.position.x,this.position.y,this.width,this.height) ;
+    }
+}
+
+//player object
+let player1 =new Player();
+console.log(player1);
+//call create player
+player1.create();
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 <h1>project path</h1>
 
 <ul>
-<li> project setup </li>
-<li> player creation</li>
+<li> project setup  -->DONE</li>
+<li> player creation  -->DONE</li>
 <li> physic(gravity,..)</li>
 <li> player movement </li>
 <li> platform + background</li>
